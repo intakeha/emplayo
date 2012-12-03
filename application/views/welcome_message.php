39f640698f13734889072a23ce6f157344d1e70d
Chonabot Intakeha <intakeha@gmail.com>
2012-12-03 01:35:59 -0800

    Update welcome message to Emplayo Dev Environment - Checking to see if the service hooks are in place

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter.Work.Live.Play1</title>
+	<title>Welcome to Emplayo</title>
 
 	<style type="text/css">
 
@@ -67,7 +67,7 @@
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Welcome to Emplayo Dev Environment</h1>
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
