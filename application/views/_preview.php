241cb9f171ee624219bc773a79bb8678cd8fca02
Chonabot Intakeha <intakeha@gmail.com>
2013-01-13 20:56:30 -0800

    Added Preview file

---
+++ application/views/_preview.php
@@ -0,0 +1,14 @@
+<div id="preview">
+	<div class="content">
+		<p id="title">Here are your results!</p>
+		<p>You have <span>20 companies</span> in your list. Here are the first few...</p>
+		<p><img src="<?php echo base_url() ?>assets/images/preview.png"></img><p>
+		<p id="info">Based on your answers, we have customized a list of companies<br>that fit <font>your</font> work-life-play
+		preferences.  Sign in or create a free account<br>to see the entire list and immediately apply for jobs.</p>
+		<div id="previewButtons">
+			<div id="signUp">Sign Up</div>
+			<div id="progressIcon"><img src="<?php echo base_url() ?>assets/images/progressIcon.png"></img></div>
+			<div id="signIn">Sign In</div>
+		</div>
+	</div>
+</div>
