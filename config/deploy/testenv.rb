8e221b4f52c72612a704417e11bc97fb1546ec6e
Hip <hipnoddic@mac.com>
2013-01-13 17:26:53 -0800

    more config updates for test env

--- config/deploy/testenv.rb
+++ config/deploy/testenv.rb
@@ -1,3 +1,13 @@
 server "emplayo.com", :app, :web, :db, :primary => true
 set :deploy_to, "/var/www/vhosts/emplayo.com/test"
 set :branch, 'testing'
+
+# This task does post deploy configuration for the target environment 
+task :post_deploy_config, :roles => :web do
+  run "cp #{current_path}/application/config/environment.config.testing #{current_path}/application/config/environment.config"
+
+end
+ 
+# After deployment has successfully completed
+# perform the post deploy config task(s)
+after "deploy:finalize_update", :post_deploy_config
