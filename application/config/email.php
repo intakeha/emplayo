69662a81e474730a96e2e3eabd69f8972a0c10f0
Hip <hipnoddic@mac.com>
2013-07-12 23:22:42 -0700

    added email config file for using mandrill service

---
+++ application/config/email.php
@@ -0,0 +1,18 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------
+| EMAIL SENDING SETTINGS
+| -------------------------------------------------------------------
+*/
+
+$config['protocol'] = 'smtp';
+$config['charset'] = 'utf-8';
+$config['mailtype'] = 'html';
+$config['smtp_timeout'] = 5;
+$config['smtp_host'] = 'smtp.mandrillapp.com';
+$config['smtp_port'] = 587;
+$config['smtp_user'] = 'hipnoddic@mac.com';
+$config['smtp_pass'] = 'EJkV_IqmhO-6El5R7IuNnA'; // It's your API key
+
+/* End of file email.php */
+/* Location: ./application/config/email.php */
