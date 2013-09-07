b4c983929d761dc802ff950475a5255cea7614c5
Chonabot Intakeha <intakeha@gmail.com>
2013-09-07 16:48:51 -0700

    Update welcome email to include start survey and see company matches

--- application/views/emails/welcome.tpl.php
+++ application/views/emails/welcome.tpl.php
@@ -6,13 +6,24 @@
   </tr>
   <tr>
     <td><div style="color:#258CD1; font-weight: bold; font-size:20px; margin-top:15px;">Welcome to Emplayo!</div>
-      <p>Share what you want in an ideal work environment, and we'll help you find the companies that fit you best.</p>
-      <p>Start now by going through the Work.Life.Play preferences</p>
-      <?php echo anchor('inquire', 'Get Started', 'style="display: block; width: 150px; height: 20px; background-color: #258CD1; color: #fff; padding: 10px; font-weight: bold; text-align: center; text-decoration: none; margin: 0 auto;"');?>
-      <p>Or paste the following into your browser:</p>
-      <div style="margin-bottom:15px;"><?php echo anchor('inquire', base_url().'inquire');?></div>
-    </td>
+      <p>Share what you want in an ideal work environment, and we'll help you find the companies that fit you best. </p>
+      </td>
   </tr>
+  <tr>
+	<td>
+		<p>If you haven't gone through the Work.Life.Play preferences, you can start now by clicking :</p>
+		<?php echo anchor('inquire', 'Get Started', 'style="display: block; width: 150px; height: 20px; background-color: #258CD1; color: #fff; padding: 10px; font-weight: bold; text-align: center; text-decoration: none; margin: 0 auto;"');?>
+		<p>Or paste the following into your browser:</p>
+		<div style="margin-bottom:15px;"><?php echo anchor('inquire', base_url().'inquire');?></div>
+	</td>
+	<td>
+		<p>To see the list of companies that fit you best, click below:
+		<?php echo anchor('login', 'Matches', 'style="display: block; width: 150px; height: 20px; background-color: #258CD1; color: #fff; padding: 10px; font-weight: bold; text-align: center; text-decoration: none; margin: 0 auto;"');?>
+		<p>Or paste the following into your browser:</p>
+		<div style="margin-bottom:15px;"><?php echo anchor('login', base_url().'login');?></div>
+		</p>
+	</td>
+</tr>
   <tr>
     <td style="background-color: #6d7a83; padding:5px 30px; color: #fff; font-size: 11px; text-align: center;">&copy;2013 Emplayo, Inc. | All Rights Reserved</td>
   </tr>
