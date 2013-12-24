dd1aa6b402cfc0d17be0ec52347f883005a2f641
Chonabot Intakeha <chon@emplayo.com>
2013-12-24 23:58:36 +1100

    Update text for ambassador page.

--- application/views/pages/_ambassadors.php
+++ application/views/pages/_ambassadors.php
@@ -13,20 +13,19 @@
 <div id="ambassadors">
 	<div class="content">
 		<div>
-			<h1 class="titles blue">Student Ambassador Program</h1>
+			<h1 class="titles blue">Student Ambassadors</h1>
 			<p>Emplayo is committed to growing and preparing the next generation of leaders for the future workforce.</p>
 			<p>The Emplayo Student Ambassador Program is an opportunity for active students to experience entrepreneurship
 			at their universities and to prepare for a successful start in their careers. The program is designed to enhance professional
-			development and soft skills, to meet and collaborate with other student ambassadors around the world, and to have fun creating
-			a legacy for their university.</p>
+			development and soft skills, to meet and collaborate with other student ambassadors around the world, and to have fun while creating
+			a legacy at their alma mater.</p>
 			<p>Every student ambassador will have an opportunity to:</p>
 			<ul>
 				<li>Gain experience in marketing, promotion and entrepreneurship</li>
 				<li>Network with other student ambassadors around the world</li>
-				<li>Strengthen resume with relevant experiences</li>
-				<li>Obtain references for job applications</li>
-				<li>Be recognized as a university ambassador on Emplayo's website</li>
-				<li>Be a part of Emplayo's Student Ambassador Alumni Network</li>
+				<li>Strengthen his/her resume with real-life experiences</li>
+				<li>Obtain references for future job applications</li>
+				<li>Be a part of Emplayo's Student Ambassador Network</li>
 			</ul>
 			<a id="button" href="/contact">Apply</a>
 		</div>		
